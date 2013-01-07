00d350df5d215fa75ee105d8c7e27b97d408ce5a
Cris Sandoval <c.sand@verizon.net>
2013-01-06 16:46:02 -0800

    Added a comment

--- EGIT_sample_02/src/main/java/EGIT_sample_02_main.java
+++ EGIT_sample_02/src/main/java/EGIT_sample_02_main.java
@@ -5,6 +5,7 @@ public class EGIT_sample_02_main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// this is main
 		System.out.println("hello");
 		System.out.println("good bye");
 
